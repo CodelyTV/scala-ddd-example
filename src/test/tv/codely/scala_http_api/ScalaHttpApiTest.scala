61d0350def9b79e2dac0e2e30e047fb017cdd089
Javier Ferrer <javier.mailserio@gmail.com>
2017-12-01 01:23:36 +0100

    Explicitly add the Akka actor and Akka stream dependency in order to have the 2.5 version of Akka actor and do not make binary incompatible conflicts with the underlying akka stream. More info: https://doc.akka.io/docs/akka-http/current/scala/http/compatibility-guidelines.html#akka-http-10-0-x-with-akka-2-5-x

--- src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
+++ src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.{ Matchers, WordSpec }
 import akka.http.scaladsl.server.Directives._
 
 final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
-  private val routes =
+  private val routesWithDefinedResponses =
     get {
       path("status") {
         complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
@@ -15,8 +15,16 @@ final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures wi
     }
 
   "ScalaHttpApi" should {
+
+    /**
+      * This is a really dummy test because with it we're testing nothing but Akka HTTP routing system.
+      * As you can see in the routesWithDefinedResponses defined above, we've already provided an implementation.
+      *
+      * However, this is useful to start digging a little in how Akka HTTP testkit works and so on.
+      * More information: https://doc.akka.io/docs/akka-http/current/scala/http/routing-dsl/testkit.html
+      */
     "respond successfully while requesting its status" in {
-      Get("/status") ~> routes ~> check {
+      Get("/status") ~> routesWithDefinedResponses ~> check {
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
         entityAs[String] shouldBe """{"status":"ok"}"""
