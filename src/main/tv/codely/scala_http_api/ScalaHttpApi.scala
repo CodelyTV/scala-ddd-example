a889a3aab9633e6bd796c0b0538dde0f1a3098fc
Rafa Gómez <rgomezcasas@gmail.com>
2017-12-02 20:05:28 +0100

    Extract routes to an object
    In order to be able to test it

--- src/main/tv/codely/scala_http_api/ScalaHttpApi.scala
+++ src/main/tv/codely/scala_http_api/ScalaHttpApi.scala
@@ -5,10 +5,7 @@ import scala.io.StdIn
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import akka.http.scaladsl.server.Directives._
 
 object ScalaHttpApi {
   def main(args: Array[String]): Unit = {
@@ -16,13 +13,7 @@ object ScalaHttpApi {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val routes: Route = get {
-      path("status") {
-        complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
-      }
-    }
-
-    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(Routes.all, "localhost", 8080)
 
     // let it run until user presses return
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
