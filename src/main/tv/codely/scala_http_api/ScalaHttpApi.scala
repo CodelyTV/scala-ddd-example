3a3e8de7f461ba3850b8c62f314a97350fab25b1
Rafa Gómez <rgomezcasas@gmail.com>
2017-12-02 19:59:54 +0100

    Add status endpoint

--- src/main/tv/codely/scala_http_api/ScalaHttpApi.scala
+++ src/main/tv/codely/scala_http_api/ScalaHttpApi.scala
@@ -1,7 +1,35 @@
 package tv.codely.scala_http_api
 
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+
 object ScalaHttpApi {
   def main(args: Array[String]): Unit = {
-    println("Hello!")
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+    val routes: Route = get {
+      path("status") {
+        complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
+      }
+    }
+
+    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
+
+    // let it run until user presses return
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine()
+
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
   }
 }
