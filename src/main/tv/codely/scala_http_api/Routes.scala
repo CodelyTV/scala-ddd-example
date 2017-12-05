eb0958b353331581cd67155086149510fd32bce0
Javier Ferrer <javier.mailserio@gmail.com>
2017-12-05 11:25:13 +0100

    Implement `ping` endpoint to make the test pass. Take note of the "~"!

--- src/main/tv/codely/scala_http_api/Routes.scala
+++ src/main/tv/codely/scala_http_api/Routes.scala
@@ -2,12 +2,15 @@ package tv.codely.scala_http_api
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.Directives.{complete, get, path}
+import akka.http.scaladsl.server.Directives._
 
 object Routes {
   val all: Route = get {
     path("status") {
       complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
+    } ~
+    path("ping") {
+      complete(HttpEntity(ContentTypes.`application/json`, """{"data":"pong"}"""))
     }
   }
 }
