0834e906972a6b0e5c4d03182fc03c02953e9d2c
Javier Ferrer <javier.mailserio@gmail.com>
2017-12-05 11:52:48 +0100

    Implement users endpoint with raw data

--- src/main/tv/codely/scala_http_api/Routes.scala
+++ src/main/tv/codely/scala_http_api/Routes.scala
@@ -1,16 +1,33 @@
 package tv.codely.scala_http_api
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 
 object Routes {
+  private val systemUsers =
+    """{
+      |    "data":[
+      |        {
+      |            "id":"123",
+      |            "name":"Edufasio"
+      |        },
+      |        {
+      |            "id":"456",
+      |            "name":"Edonisio"
+      |        }
+      |    ]
+      |}""".stripMargin
+
   val all: Route = get {
     path("status") {
       complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
     } ~
-    path("ping") {
-      complete(HttpEntity(ContentTypes.`application/json`, """{"data":"pong"}"""))
-    }
+      path("ping") {
+        complete(HttpEntity(ContentTypes.`application/json`, """{"data":"pong"}"""))
+      } ~
+      path("users") {
+        complete(HttpEntity(ContentTypes.`application/json`, systemUsers))
+      }
   }
 }
--- src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
+++ src/test/tv/codely/scala_http_api/ScalaHttpApiTest.scala
@@ -2,8 +2,8 @@ package tv.codely.scala_http_api
 
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Matchers, WordSpec}
 
 final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest {
   "ScalaHttpApi" should {
@@ -20,6 +20,28 @@ final class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures wi
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
         entityAs[String] shouldBe """{"data":"pong"}"""
+      }
+    }
+
+    "return all the system users" in {
+      Get("/users") ~> Routes.all ~> check {
+        val expectedSystemUsers =
+          """{
+            |    "data":[
+            |        {
+            |            "id":"123",
+            |            "name":"Edufasio"
+            |        },
+            |        {
+            |            "id":"456",
+            |            "name":"Edonisio"
+            |        }
+            |    ]
+            |}""".stripMargin
+
+        status shouldBe StatusCodes.OK
+        contentType shouldBe ContentTypes.`application/json`
+        entityAs[String] shouldBe expectedSystemUsers
       }
     }
   }
