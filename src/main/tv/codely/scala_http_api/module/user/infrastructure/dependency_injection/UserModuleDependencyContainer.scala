d07c7c906c990e2a0a4ecc8ffd7eb122d0dab3db
Rafa Gómez <rgomezcasas@gmail.com>
2017-12-06 21:51:46 +0100

    Test video repository

--- src/main/tv/codely/scala_http_api/module/user/infrastructure/dependency_injection/UserModuleDependencyContainer.scala
+++ src/main/tv/codely/scala_http_api/module/user/infrastructure/dependency_injection/UserModuleDependencyContainer.scala
@@ -4,7 +4,7 @@ import tv.codely.scala_http_api.module.user.application.search.UsersSearcher
 import tv.codely.scala_http_api.module.user.infrastructure.repository.InMemoryUserRepository
 
 final class UserModuleDependencyContainer {
-  private val repository = new InMemoryUserRepository
+  val repository = new InMemoryUserRepository
 
   val usersSearcher = new UsersSearcher(repository)
 }
---
+++ src/test/tv/codely/scala_http_api/module/IntegrationTestCase.scala
@@ -0,0 +1,5 @@
+package tv.codely.scala_http_api.module
+
+protected[scala_http_api] trait IntegrationTestCase extends UnitTestCase {
+
+}
