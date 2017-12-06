ea824fd5fb7727c59aec6cc314d76218b8956cfc
Rafa Gómez <rgomezcasas@gmail.com>
2017-12-06 13:10:27 +0100

    Extract video repository

--- src/main/tv/codely/scala_http_api/module/video/infrastructure/dependency_injection/VideoModuleDependencyContainer.scala
+++ src/main/tv/codely/scala_http_api/module/video/infrastructure/dependency_injection/VideoModuleDependencyContainer.scala
@@ -1,7 +1,10 @@
 package tv.codely.scala_http_api.module.video.infrastructure.dependency_injection
 
 import tv.codely.scala_http_api.module.video.application.VideosSearcher
+import tv.codely.scala_http_api.module.video.infrastructure.repository.InMemoryVideoRepository
 
 final class VideoModuleDependencyContainer {
-  val videosSearcher = new VideosSearcher
+  private val repository = new InMemoryVideoRepository
+
+  val videosSearcher = new VideosSearcher(repository)
 }
