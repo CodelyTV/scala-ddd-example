9250346601fd66b0d689c69e7e422e046576c0b9
Javier Ferrer <javier.mailserio@gmail.com>
2017-12-08 14:11:20 +0100

    Add database tables definition

---
+++ database/users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+  id         BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id    CHAR(36)            NOT NULL,
+  name       VARCHAR(255)        NOT NULL,
+  updated_at TIMESTAMP(3)        NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (id),
+  UNIQUE KEY u_user_id (user_id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
