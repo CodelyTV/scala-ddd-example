da85e108f38a8ef2ec699eae701ee56f85993b6e
Javier Ferrer <javier.mailserio@gmail.com>
2017-12-08 11:48:38 +0100

    Split route directives into several values for readability

--- src/main/tv/codely/scala_http_api/entry_point/Routes.scala
+++ src/main/tv/codely/scala_http_api/entry_point/Routes.scala
@@ -1,19 +1,25 @@
 package tv.codely.scala_http_api.entry_point
 
-import spray.json.DefaultJsonProtocol._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import spray.json.DefaultJsonProtocol._
 import spray.json.JsValue
+
 import scala.concurrent.duration._
 
 final class Routes(container: EntryPointDependencyContainer) {
-  val all: Route =
-    get {
-      path("status")(container.statusGetController.get()) ~
-      path("users")(container.userGetController.get()) ~
-      path("videos")(container.videoGetController.get())
-    } ~
+  private val status = get {
+    path("status")(container.statusGetController.get())
+  }
+
+  private val user = get {
+    path("users")(container.userGetController.get())
+  }
+
+  private val video = get {
+    path("videos")(container.videoGetController.get())
+  } ~
     post {
       path("videos") {
         jsonBody { body =>
@@ -26,6 +32,8 @@ final class Routes(container: EntryPointDependencyContainer) {
         }
       }
     }
+
+  val all: Route = status ~ user ~ video
 
   private def jsonBody(handler: Map[String, JsValue] => Route): Route =
     entity(as[JsValue])(json => handler(json.asJsObject.fields))
