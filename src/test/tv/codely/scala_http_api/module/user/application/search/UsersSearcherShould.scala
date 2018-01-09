fb44e2042c54995a1fa9d32168fa960012a40bec
Javier Ferrer <javier.mailserio@gmail.com>
2018-01-09 17:08:13 +0100

    Replace the unified `UserUnitTestCase` for multiple inheritance from isolated traits for each mock. Why: * Explicitness: Since our final unit tests would have to extend from each one of the used traits, we could see where do the mocks come from. * Decoupling/SRP/maintainability: This way each mock has its very own trait making it easier to add more behaviour without ending up with multiple method in the very same class

--- src/test/tv/codely/scala_http_api/module/user/application/search/UsersSearcherShould.scala
+++ src/test/tv/codely/scala_http_api/module/user/application/search/UsersSearcherShould.scala
@@ -1,9 +1,10 @@
 package tv.codely.scala_http_api.module.user.application.search
 
-import tv.codely.scala_http_api.module.user.UserUnitTestCase
+import tv.codely.scala_http_api.module.UnitTestCase
 import tv.codely.scala_http_api.module.user.domain.UserStub
+import tv.codely.scala_http_api.module.user.infrastructure.repository.UserRepositoryMock
 
-final class UsersSearcherShould extends UserUnitTestCase {
+final class UsersSearcherShould extends UnitTestCase with UserRepositoryMock {
   private val searcher = new UsersSearcher(repository)
 
   "search all existing users" in {
