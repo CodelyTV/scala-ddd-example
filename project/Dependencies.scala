ded23360475ad0b0fde83fade7462f1763a433fa
Javier Ferrer <javier.mailserio@gmail.com>
2018-01-10 12:56:36 +0100

    Add `logback-classic` Logging backend for SLF4J and `scala-logging` as a SLF4J wrapper for Scala dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,16 +7,18 @@ object Dependencies {
   }
 
   val production = Seq(
-    "com.github.nscala-time" %% "nscala-time"          % "2.18.0",
-    "com.lihaoyi"            %% "pprint"               % "0.5.3",
-    "com.typesafe.akka"      %% "akka-http"            % Versions.akkaHttp,
-    "com.typesafe.akka"      %% "akka-actor"           % Versions.akka,
-    "com.typesafe.akka"      %% "akka-stream"          % Versions.akka, // Explicit dependency due to: https://bit.ly/akka-http-25
-    "com.typesafe.akka"      %% "akka-http-spray-json" % Versions.akkaHttp,
-    "org.tpolecat"           %% "doobie-core"          % "0.5.0-M13",
-    "mysql"                  % "mysql-connector-java"  % "5.1.45",
-    "com.github.scopt"       %% "scopt"                % "3.7.0", // Command Line Commands such as de DbTablesCreator
-    "com.newmotion"          %% "akka-rabbitmq"        % "5.0.0"
+    "com.github.nscala-time"     %% "nscala-time"          % "2.18.0",
+    "com.lihaoyi"                %% "pprint"               % "0.5.3",
+    "com.typesafe.akka"          %% "akka-http"            % Versions.akkaHttp,
+    "com.typesafe.akka"          %% "akka-actor"           % Versions.akka,
+    "com.typesafe.akka"          %% "akka-stream"          % Versions.akka, // Explicit dependency due to: https://bit.ly/akka-http-25
+    "com.typesafe.akka"          %% "akka-http-spray-json" % Versions.akkaHttp,
+    "org.tpolecat"               %% "doobie-core"          % "0.5.0-M13",
+    "mysql"                      % "mysql-connector-java"  % "5.1.45",
+    "com.github.scopt"           %% "scopt"                % "3.7.0", // Command Line Commands such as de DbTablesCreator
+    "com.newmotion"              %% "akka-rabbitmq"        % "5.0.0",
+    "ch.qos.logback"             % "logback-classic"       % "1.2.3", // Logging backend implementation
+    "com.typesafe.scala-logging" %% "scala-logging"        % "3.7.2" // SLF4J Scala wrapper
   )
 
   val test = Seq(
