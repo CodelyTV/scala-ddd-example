c93f269a6ea224336259e573681aeb4ff90bf636
Javier Ferrer <javier.mailserio@gmail.com>
2018-01-11 13:33:49 +0100

    Add Scala compiler options in order to be more strict and have more clues. More information: https://tpolecat.github.io/2017/04/25/scalac-flags.html

--- src/main/tv/codely/scala_http_api/module/shared/domain/Logger.scala
+++ src/main/tv/codely/scala_http_api/module/shared/domain/Logger.scala
@@ -1,9 +1,9 @@
 package tv.codely.scala_http_api.module.shared.domain
 
 trait Logger {
-  def info(message: String)
-  def warn(message: String)
-  def error(message: String)
+  def info(message: String): Unit
+  def warn(message: String): Unit
+  def error(message: String): Unit
 
   def info(message: String, context: Map[String, Any]): Unit
   def warn(message: String, context: Map[String, Any]): Unit
