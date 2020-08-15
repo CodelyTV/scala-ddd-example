48caa8ada16f08c0f7bfff4ddcec6c414fa1be64
acortijo <alex.cortijo117@gmail.com>
2020-08-16 01:04:31 +0200

    First version find existing video test case

--- src/mooc/test/tv/codely/mooc/video/application/search/VideosSearcherShould.scala
+++ src/mooc/test/tv/codely/mooc/video/application/search/VideosSearcherShould.scala
@@ -1,11 +1,14 @@
 package tv.codely.mooc.video.application.search
 
-import tv.codely.mooc.video.domain.VideoMother
+import tv.codely.mooc.user.domain.UserIdMother
+import tv.codely.mooc.video.domain._
 import tv.codely.mooc.video.infrastructure.repository.VideoRepositoryMock
 import tv.codely.shared.infrastructure.unit.UnitTestCase
 
 final class VideosSearcherShould extends UnitTestCase with VideoRepositoryMock {
   private val searcher = new VideosSearcher(repository)
+//  private val publisher = mock[MessagePublisher]
+//  private val creator = new VideoCreator(repository, publisher)
 
   "search all existing videos" in {
     val existingVideo        = VideoMother.random
@@ -15,5 +18,21 @@ final class VideosSearcherShould extends UnitTestCase with VideoRepositoryMock {
     repositoryShouldFind(existingVideos)
 
     searcher.all().futureValue shouldBe existingVideos
+  }
+
+  "search an existing video" in {
+    val videoId = VideoIdMother.random
+    val videoTitle = VideoTitleMother.random
+    val videoDuration = VideoDurationMother.random
+    val videoCategory = VideoCategoryMother.random
+    val userId = UserIdMother.random
+    val existingVideo = Video(videoId, videoTitle, videoDuration, videoCategory, userId)
+
+    repositoryShouldSave(existingVideo)
+    repositoryShouldFind(Seq(existingVideo))
+
+    repository.save(existingVideo)
+    searcher.all().futureValue.find(v => v.id.equals(existingVideo.id)).get shouldBe existingVideo
+
   }
 }
